a537d30bbb585afe8c40b82882d284b3ff7f25b9
Ndi Sampson <ndisampson@gmail.com>
2018-05-20 21:34:29 -0400

    refactor sum function

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,12 +3,14 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collections of unspecified length", func(t *testing.T) {
+		numbers := []int{5, 66, 9}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 80
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
